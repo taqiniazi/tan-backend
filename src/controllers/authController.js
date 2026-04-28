e82efa91dad33293b4aa577fe659fc6c9294beaf
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-04-28 14:03:57 +0500

    Update authController.js

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -51,10 +51,10 @@ exports.signup = async (req, res) => {
 
 exports.login = async (req, res) => {
   const user = await User.findOne({ email: req.body.email });
-  if (!user) return res.status(400).send("User not found");
+  if (!user) return res.status(400).json({ error: "User not found" });
 
   const valid = await bcrypt.compare(req.body.password, user.password);
-  if (!valid) return res.status(400).send("Invalid password");
+  if (!valid) return res.status(400).json({ error: "Invalid password" });
 
   // Update IP and Device ID on login
   user.ipAddress = req.headers['x-forwarded-for'] || req.socket.remoteAddress;
