5ba513b6b2cd0b2cd9f83f56cda41cf4d8f4b4ce
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-04-28 14:25:02 +0500

    Update server.js

--- server.js
+++ server.js
@@ -1,4 +1,8 @@
 const app = require("./src/app");
+// Initialize Workers
+require("./src/workers/miningWorker");
+require("./src/workers/payoutWorker");
+require("./src/workers/worker");
 
 app.listen(process.env.PORT, "0.0.0.0", () => {
   console.log(`Server running at http://10.50.47.88:${process.env.PORT}`);
