d560251304057e477568dfbbbb093a062107e9bf
Taqi Raza <taqi.raza@pitb.gov.pk>
2026-05-04 16:31:55 +0500

    Update rateLimiter.js

--- src/middlewares/rateLimiter.js
+++ src/middlewares/rateLimiter.js
@@ -3,7 +3,7 @@ const logger = require('../utils/logger');
 
 const apiLimiter = rateLimit({
   windowMs: 15 * 60 * 1000, // 15 minutes
-  max: 100, // Limit each IP to 100 requests per windowMs
+  max: 1000, // Limit each IP to 1000 requests per windowMs
   standardHeaders: true, // Return rate limit info in the `RateLimit-*` headers
   legacyHeaders: false, // Disable the `X-RateLimit-*` headers
   handler: (req, res, next, options) => {
